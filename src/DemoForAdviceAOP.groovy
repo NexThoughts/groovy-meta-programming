2999dd15b843f7bc36aae74b6ddc3128257a68e6
chetan <bairagisirfire@gmail.com>
2016-03-13 20:38:27 +0530

    first commit

---
+++ src/DemoForAdviceAOP.groovy
@@ -0,0 +1,19 @@
+
+class Person {
+
+  def run(int vv)
+  {
+    println 'you r my'+vv
+  }
+
+}
+def sam = new Person()
+println 'before'
+sam.metaClass.invokeMethod= { String name, args ->
+  println 'before'
+  def method = Person.metaClass.getMetaMethod('run', 10)
+   method.invoke(delegate,10)
+
+}
+
+sam.run(10)
